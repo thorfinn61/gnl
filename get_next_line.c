95781d51dd9e89e58b8947cd6a16bc1c736a4a8f
thorfinn <e.sahinnn61@gmail.com>
2025-11-17 12:42:37 +0100

    finish

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,82 @@
 /*   By: elsahin <elsahin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 11:02:55 by elsahin           #+#    #+#             */
-/*   Updated: 2025/11/12 11:02:56 by elsahin          ###   ########.fr       */
+/*   Updated: 2025/11/17 12:42:28 by elsahin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*read_and_join(int fd, char *rest)
+{
+	char	*buffer;
+	int		bytes;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchr(rest, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		rest = ft_strjoin(rest, buffer);
+	}
+	free(buffer);
+	return (rest);
+}
+
+char	*extract_line(char *rest)
+{
+	char	*line;
+	int		i;
+
+	if (!rest || rest[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	if (rest[i] == '\n')
+		i++;
+	line = ft_substr(rest, 0, i);
+	return (line);
+}
+
+char	*clean_rest(char *rest)
+{
+	char	*new_rest;
+	int		i;
+
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	if (!rest[i])
+	{
+		free(rest);
+		return (NULL);
+	}
+	i++;
+	new_rest = ft_substr(rest, i, ft_strlen(rest) - i);
+	free(rest);
+	return (new_rest);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*rest;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	rest = read_and_join(fd, rest);
+	if (!rest)
+		return (NULL);
+	line = extract_line(rest);
+	rest = clean_rest(rest);
+	return (line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: elsahin <elsahin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 11:04:16 by elsahin           #+#    #+#             */
-/*   Updated: 2025/11/12 18:14:24 by elsahin          ###   ########.fr       */
+/*   Updated: 2025/11/17 09:43:08 by elsahin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s1);
 
 #endif
